907df375827f08fa7296ea25886b6a4d0807d69f
Alex Bush <alexabush7@gmail.com>
2018-11-30 11:51:39 -0800

    built game and wrote Readme description of game

---
+++ build/precache-manifest.dfc272eb83eb0519e17894b268630550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "351fc99354337a05b0be",
+    "url": "/snake-game/static/js/runtime~main.351fc993.js"
+  },
+  {
+    "revision": "e038d885d1593828f5d7",
+    "url": "/snake-game/static/js/main.e038d885.chunk.js"
+  },
+  {
+    "revision": "bb7184833edf755097a4",
+    "url": "/snake-game/static/js/1.bb718483.chunk.js"
+  },
+  {
+    "revision": "e038d885d1593828f5d7",
+    "url": "/snake-game/static/css/main.4bdd1acb.chunk.css"
+  },
+  {
+    "revision": "10b073f448e454dbe033c5181250ab41",
+    "url": "/snake-game/index.html"
+  }
+];
