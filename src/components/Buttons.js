2916743807ad251e7e3d75ab6cbe691beed4f29b
Alex Bush <alexabush7@gmail.com>
2018-12-01 09:18:08 -0800

    reorganized functionality within css files to make more sense and be better organized

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,28 +5,16 @@ class Buttons extends Component {
   render() {
     return (
       <div className="buttons-container">
-        <button
-          className="btn btn-primary button"
-          onClick={this.props.handleBtnPress}
-        >
+        <button className="btn btn-primary" onClick={this.props.handleBtnPress}>
           Up
         </button>
-        <button
-          className="btn btn-primary button"
-          onClick={this.props.handleBtnPress}
-        >
+        <button className="btn btn-primary" onClick={this.props.handleBtnPress}>
           Down
         </button>
-        <button
-          className="btn btn-primary button"
-          onClick={this.props.handleBtnPress}
-        >
+        <button className="btn btn-primary" onClick={this.props.handleBtnPress}>
           Left
         </button>
-        <button
-          className="btn btn-primary button"
-          onClick={this.props.handleBtnPress}
-        >
+        <button className="btn btn-primary" onClick={this.props.handleBtnPress}>
           Right
         </button>
       </div>
